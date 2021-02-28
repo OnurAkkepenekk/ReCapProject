f0ad40c73e7a949687d325313bcf87016e2c819c
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-02-28 03:31:22 +0300

    AOP and ValidationAspect
    Add AOP and ValidationAspect

--- ReCapProject/Business/Concrete/BrandManager.cs
+++ ReCapProject/Business/Concrete/BrandManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
@@ -19,7 +20,7 @@ namespace Business.Concrete
         {
             _brandDal = brandDal;
         }
-
+        [ValidationAspect(typeof(BrandValidator))]
         public IResult Add(Brand brand)
         {
             ValidationTool.Validate(new BrandValidator(), brand);
